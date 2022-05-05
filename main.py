4d5e4e0e7daa62ca3300959563d3395648a9ce25
Marcus Chong <kok_yung@hotmail.com>
2022-05-05 15:27:06 +0800

    Add a new function in main.py - Yesterday I was happy

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
 print("Today is a good day.")
 print("Today is Thursday")
 print("Today I am happy")
+print("Yesterday I was happy")
 print("Today I ate bread for my breakfast")
 print("Today I ate rice for my lunch")
