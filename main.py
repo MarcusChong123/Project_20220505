b1021a6383c00f4bb3b94b6a4ccd0a8a9ee2ce1c
Marcus Chong <kok_yung@hotmail.com>
2022-05-05 15:25:34 +0800

    Added new function into main.py - Yesterday I was sad

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
 print("Today is a good day.")
 print("Today is Thursday")
 print("Today I am happy")
+print("Yesterday I was sad")
 print("Today I ate bread for my breakfast")
 print("Today I ate rice for my lunch")
